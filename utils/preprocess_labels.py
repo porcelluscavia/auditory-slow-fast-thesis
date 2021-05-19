6afdc5b1449a54704dc2a42e7b134c3e6aebed55
Sam Tureski <gpiggies@gmail.com>
2021-05-19 02:06:41 +0200

    added BERT sentence transformer

--- utils/preprocess_labels.py
+++ utils/preprocess_labels.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 import gensim.downloader as api
 from gensim.models import KeyedVectors
-
-
+from sentence_transformers import SentenceTransformer
 
 
 def get_mean_vector(word2vec_model, words):
+    # Calculate mean vector of label.
     # source of this code: http://yaronvazana.com/2018/09/20/average-word-vectors-generate-document-paragraph-sentence-embeddings/
     # remove out-of-vocabulary words
     words = [word for word in words if word in word2vec_model.vocab]
@@ -16,32 +16,58 @@ def get_mean_vector(word2vec_model, words):
         return []
 
 
+def make_word2vec_file(filename, model, labels):
+    # Get mean word2vec vector for all labels and write them to a file.
+
+    kv = KeyedVectors(vector_size=model.wv.vector_size)
+    vec_id_list = range(0, len(labels))
+
+    vectors = []
+    for label in labels:
+        vec = get_mean_vector(model, label)
+        vectors.append(vec)
+    kv.add(vec_id_list, vectors)
+    kv.save_word2vec_format(filename, binary=False)
+    return
+
+
+def get_sentence_bert(bert_sent_model, labels):
+    # Get a single vector for each label which preserves semantic and contextual information.
+
+    bert_sent_model = SentenceTransformer(bert_sent_model)
+    sentence_embeddings = bert_sent_model.encode(labels)
+    return sentence_embeddings
+
+def make_bert_sentence_file(filename, bert_sent_model, labels, vec_size=100):
+    embeddings = get_sentence_bert(labels)
+    kv = KeyedVectors(vector_size=vec_size)
+    vec_id_list = range(0, len(labels))
+
+    kv.add(vec_id_list, embeddings)
+    kv.save_word2vec_format(filename, binary=False)
+    return
 
 
 
 def main():
-    DATA_PATH = '/Users/samski/Downloads/test.pkl'
-    OUTFILE_PATH = "word2vec_outfile_test.txt"
+    # INPUT_PATH = '/Users/samski/Downloads/test.pkl'
+    # OUTFILE_PATH = 'word2vec_outfile_test.txt'
+    INPUT_PATH = '/Users/samski/Downloads/train.pkl'
+    OUTFILE_PATH = 'word2vec_outfile_train.txt'
 
-    model = api.load("glove-wiki-gigaword-100")
+    word2vec_model = api.load('glove-wiki-gigaword-100')
+    bert_sent_model = 'stsb-mpnet-base-v2'
 
-    object = pd.read_pickle(r'/Users/samski/Downloads/test.pkl')
+    object = pd.read_pickle(INPUT_PATH)
 
     object[['class']] = object['class'].str.split()
-    desc_list = object['class'].tolist()
+    labels_list = object['class'].tolist()
 
-    kv = KeyedVectors(vector_size=model.wv.vector_size)
+    make_word2vec_file(OUTFILE_PATH, word2vec_model, labels_list)
 
-    vec_id_list = range(0, len(desc_list))
+    embedding = get_sentence_bert(bert_sent_model, labels_list[0])
+    print(embedding.shape)
 
-
-    vectors = []
-    for label in desc_list:
-        vec = get_mean_vector(model, label)
-        vectors.append(vec)
-
-    kv.add(vec_id_list, vectors)
-    kv.save_word2vec_format(OUTFILE_PATH, binary=False)
 
 
 

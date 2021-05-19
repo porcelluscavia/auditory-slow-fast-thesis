23e3a4a0246ed432028144438c119a96cef5662d
Sam Tureski <gpiggies@gmail.com>
2021-05-19 20:41:41 +0200

    code to successfully make embeddings

--- utils/preprocess_labels.py
+++ utils/preprocess_labels.py
@@ -4,6 +4,22 @@ import gensim.downloader as api
 from gensim.models import KeyedVectors
 from sentence_transformers import SentenceTransformer
 
+##WARNING! Some of the sentence-bert models will not load in properly. I think a problem with the torch/transformers install on my computer.
+
+def process_labels(input_pickle_file, model_is_bert=True):
+    # Returns the multi-word labels from the data as arrays of strings.
+    # Sent bert model needs input as single multiword strings; word2vec needs arrays of single-word strings.
+
+    object = pd.read_pickle(input_pickle_file)
+
+    if model_is_bert:
+        labels_list = object['class'].tolist()
+    else:
+        object[['class']] = object['class'].str.split()
+        labels_list = object['class'].tolist()
+
+    return labels_list
+
 
 def get_mean_vector(word2vec_model, words):
     # Calculate mean vector of label.
@@ -33,16 +49,18 @@ def make_word2vec_file(filename, model, labels):
 
 def get_sentence_bert(bert_sent_model, labels):
     # Get a single vector for each label which preserves semantic and contextual information.
-
-    bert_sent_model = SentenceTransformer(bert_sent_model)
-    sentence_embeddings = bert_sent_model.encode(labels)
+    #add ability to change model
+    # model = SentenceTransformer('average_word_embeddings_glove.6B.300d')
+    model = SentenceTransformer(bert_sent_model)
+    sentence_embeddings = model.encode(labels)
     return sentence_embeddings
 
-def make_bert_sentence_file(filename, bert_sent_model, labels, vec_size=100):
-    embeddings = get_sentence_bert(labels)
+
+def make_bert_sentence_file(filename, bert_sent_model, labels, vec_size=300):
+    #Get all the
+    embeddings = get_sentence_bert(bert_sent_model, labels)
     kv = KeyedVectors(vector_size=vec_size)
     vec_id_list = range(0, len(labels))
-
     kv.add(vec_id_list, embeddings)
     kv.save_word2vec_format(filename, binary=False)
     return
@@ -50,24 +68,39 @@ def make_bert_sentence_file(filename, bert_sent_model, labels, vec_size=100):
 
 
 def main():
-    # INPUT_PATH = '/Users/samski/Downloads/test.pkl'
-    # OUTFILE_PATH = 'word2vec_outfile_test.txt'
-    INPUT_PATH = '/Users/samski/Downloads/train.pkl'
-    OUTFILE_PATH = 'word2vec_outfile_train.txt'
+    #toggle me!
+    train = False
+    # bert = False
+    bert = True
+    # train = True
 
-    word2vec_model = api.load('glove-wiki-gigaword-100')
-    bert_sent_model = 'stsb-mpnet-base-v2'
+    if train:
+        INPUT_PATH = '/Users/samski/Downloads/train.pkl'
 
-    object = pd.read_pickle(INPUT_PATH)
+    else:
+        INPUT_PATH = '/Users/samski/Downloads/test.pkl'
 
-    object[['class']] = object['class'].str.split()
-    labels_list = object['class'].tolist()
 
-    make_word2vec_file(OUTFILE_PATH, word2vec_model, labels_list)
 
-    embedding = get_sentence_bert(bert_sent_model, labels_list[0])
-    print(embedding.shape)
+    if bert:
+        if train:
+            OUTFILE_PATH = 'bert_outfile_train.txt'
+        else:
+            OUTFILE_PATH = 'bert_outfile_test.txt'
 
+        labels_list = process_labels(INPUT_PATH, model_is_bert=True)
+
+        # # bert_sent_model = 'stsb-mpnet-base-v2' #doesn't work due to probable bad caching in torch!
+        bert_sent_model = 'average_word_embeddings_glove.6B.300d'
+        make_bert_sentence_file(OUTFILE_PATH, bert_sent_model, labels_list)
+    else:
+        if train:
+            OUTFILE_PATH = 'word2vec_outfile_train_new.txt'
+        else:
+            OUTFILE_PATH = 'word2vec_outfile_test.txt'
+        word2vec_model = api.load('glove-wiki-gigaword-100')
+        labels_list = process_labels(INPUT_PATH, model_is_bert=False)
+        make_word2vec_file(OUTFILE_PATH, word2vec_model, labels_list)
 
 
 
